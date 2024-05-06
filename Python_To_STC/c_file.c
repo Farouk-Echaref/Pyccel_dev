21e3fe9cca83dff86388e0e44e34144570e841a5
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-05-06 12:26:50 +0100

    possible new declaration format for STC containers

--- Python_To_STC/c_file.c
+++ Python_To_STC/c_file.c
@@ -1,15 +1,18 @@
 # include "c_header_file.h"
 
 bool initialised;
-
+vec_int a;
+vec_float b;
 /*........................................*/
 
 void declare_init(void) 
 {
     if (!initialised)
     {
-        vec_int a = {0};  // Using vec_int defined in c_header_file.h
-        vec_float b = {0};  // Using vec_float defined in c_header_file.h
+        a = vec_int_init();
+        b = vec_float_init();
+        // vec_int a = {0};  // Using vec_int defined in c_header_file.h
+        // vec_float b = {0};  // Using vec_float defined in c_header_file.h
 
         vec_int_push(&a, 1);
         vec_float_push(&b, 2);
@@ -20,7 +23,8 @@ void declare_init(void)
 
 /*........................................*/
 
-void declare__free(vec_int *a, vec_int *b)
+// void declare__free(vec_int *a, vec_int *b)
+void declare_free()
 {
     if (initialised)
     {
